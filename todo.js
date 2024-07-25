727a76c0b2de6fd78cc4b2c0c66f2957cb72f0e2
nesgnas <sangbdt102@gmail.com>
2024-07-25 14:13:57 +0700

    add renderTodo

--- todo.js
+++ todo.js
@@ -52,4 +52,55 @@ class TodoList {
         }
         this.renderTodos(currentFilter);
     }
+
+    clearTodoInput() {
+        const todoInput = document.getElementById("newTodo");
+        todoInput.value = "";
+    }
+
+    renderTodos(filter = 'all') {
+        const todoList = document.getElementById('todoList');
+        todoList.innerHTML = '';
+
+        let filteredTodos = this.todos;
+        if (filter === 'done') {
+            filteredTodos = this.todos.filter(todo => todo.isDone);
+        } else if (filter === 'undone') {
+            filteredTodos = this.todos.filter(todo => !todo.isDone);
+        }
+
+        filteredTodos.forEach(todo => {
+            const todoItem = document.createElement('li');
+            todoItem.className = 'todo-item';
+
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.checked = todo.isDone;
+            checkbox.onchange = () => this.toggleTodo(todo.id, filter);
+
+            const text = document.createElement('input');
+            text.type = 'text';
+            text.value = todo.text;
+            text.readOnly = true;
+
+            const editTodoBtn = document.createElement('button');
+            editTodoBtn.textContent = 'Edit';
+            editTodoBtn.onclick = () => this.editTodoText(todo.id);
+
+            const deleteTodoBtn = document.createElement('button');
+            deleteTodoBtn.textContent = 'Delete';
+            deleteTodoBtn.onclick = () => this.deleteTodo(todo.id);
+
+            todoItem.appendChild(checkbox);
+            todoItem.appendChild(text);
+            todoItem.appendChild(editTodoBtn);
+            todoItem.appendChild(deleteTodoBtn);
+            todoList.appendChild(todoItem);
+        });
+    }
+
 }
+
+const todoList = new TodoList();
+document.getElementById("addTodoBtn").addEventListener("click", () => todoList.addTodo());
+document.getElementById("filter").addEventListener("change", () => todoList.filterTodos());
